d3e909adb3a5b840cffb27aacd651dd036488e44
guitarkeegan <keegananglim@gmail.com>
2022-07-03 07:20:28 -0700

    add get route for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,18 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const allCategories = await  Category.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(allCategories);
+  }
+  catch {
+    res.status(500).json({message: "Something wend wrong"});
+  }
   
 });
 
