050f053409ddd9ea66bd78b8de1af45828f09440
guitarkeegan <keegananglim@gmail.com>
2022-07-04 13:35:43 -0700

    add put for tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,14 +38,57 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create({tag_name: req.body.tag_name});
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
-});
+  try {
+    const updatedTag = await Tag.update(
+      {
+        name: req.body.name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!updatedTag){
+      res.status(404).json("No tag found with that ID");
+      return;
+    }
+    res.json(updatedTag);
+  } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
